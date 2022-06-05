0802ab3293675dfc85f56270df446fc0b5f397a2
Tharindu Lakmal <tharindu96221@gmail.com>
2022-06-05 20:10:16 +0530

    Add project files.

---
+++ MusicalProject/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace MusicalProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MusicalProject.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MusicalProject.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
